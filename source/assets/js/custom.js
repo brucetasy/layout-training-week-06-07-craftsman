8c2efd78d6cbb64bad76bcfb42a8cbe585f7e0d5
Jimmy <gmwuwork@gmail.com>
2020-06-01 10:50:45 +0800

    更新段落字級大小設定與準備處理 navbar 內頁與首頁樣式與功能

--- source/assets/js/custom.js
+++ source/assets/js/custom.js
@@ -2,15 +2,12 @@ console.log('js all');
 
 $(function () {
   console.log('jquery run');
-  // $('.js-call-coverPanel').click( function (e) {
-  //   e.preventDefault();
-  //   // console.log('.js-call-coverPanel on click');
-  //   $(".js-coverPanel-section").addClass('active');
-  // });
-  // $('.js-close-coverPanel').click( function (e) {
-  //   e.preventDefault();
-  //   $(".js-coverPanel-section").removeClass('active');
-  // });
+  
+  var winHeight = $(window).height();
+  console.log('winHeight', winHeight);
+  $(window).scroll(function () {
+    console.log('$(this).scrollTop()', $(this).scrollTop());
+  });
 
   /* Fixed scrollable c-sidebar menu with a content overlay
   -------------------------------------------------- */
